bf962c9caa13ef476c8022d28384c58660b1ab2f
Kleyson Renato <kleysonwork@gmail.com>
2025-11-11 22:07:45 -0300

    Atualiza projeto staticsgame com CRUD unificado e organização

---
+++ build/resources/main/data.sql
@@ -0,0 +1,16 @@
+-- PRODUTOS
+INSERT INTO produto (nome, descricao, preco) VALUES ('Teclado Mecânico', 'Teclado mecânico RGB', 299.99);
+INSERT INTO produto (nome, descricao, preco) VALUES ('Mouse Gamer', 'Mouse gamer com DPI ajustável', 199.50);
+INSERT INTO produto (nome, descricao, preco) VALUES ('Monitor 27"', 'Monitor Full HD 27 polegadas', 1299.00);
+
+-- CARROS
+INSERT INTO carro (marca, modelo, ano, preco) VALUES ('Ford', 'Ka', 2022, 45000.0);
+INSERT INTO carro (marca, modelo, ano, preco) VALUES ('Chevrolet', 'Onix', 2023, 60000.0);
+
+-- FILMES
+INSERT INTO filme (titulo, diretor, anoLancamento, genero) VALUES ('O Poderoso Chefão', 'Francis Ford Coppola', 1972, 'Crime');
+INSERT INTO filme (titulo, diretor, anoLancamento, genero) VALUES ('Matrix', 'Lana Wachowski', 1999, 'Ficção');
+
+-- MOTOS
+INSERT INTO moto (marca, modelo, ano, cilindrada, preco) VALUES ('Honda', 'CB500', 2021, 500, 35000.0);
+INSERT INTO moto (marca, modelo, ano, cilindrada, preco) VALUES ('Yamaha', 'R3', 2022, 321, 30000.0);
